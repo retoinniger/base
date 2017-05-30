83d8724198069bbb59b1e3f91e59784df0d77c9d
Reto Inniger <reto.inniger@gmx.ch>
2017-05-30 22:21:47 +0200

    erster versuch

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
-josh = User.create! do |user|
-         user.name         = 'josh'
-         user.email        = 'josh@example.com'
-         user.password     = 'joshjosh'
+reto = User.create! do |user|
+         user.name         = 'reto'
+         user.email        = 'reto@example.com'
+         user.password     = 'lancia.'
          user.confirmed_at = Time.now
        end
 
-josh.add_role :admin
+reto.add_role :admin
 
 marc = User.create! do |user|
          user.name         = 'marc'
